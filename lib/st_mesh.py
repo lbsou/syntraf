87b39cf58d45d50159163485e207f80516974e99
Louis-Berthier Soulliere <shadow131@hotmail.com>
2022-07-16 20:59:44 -0400

    Client having trouble reconnecting

--- lib/st_mesh.py
+++ lib/st_mesh.py
@@ -945,6 +945,9 @@ def handler(_config, _dict_by_node_generated_config, conn_db, dict_of_commands_f
                 received_data = sock_rcv(sckt)
 
                 if received_data is None:
+                    received_data = sock_rcv(sckt)
+
+                if received_data is None:
                     server_log.debug(f"CONTEXT: {dict_of_clients[uid].client_uid} - INVALID DATA RECEIVED")
                     dict_of_clients[uid].status_explanation = "CONNECTION RESET BY PEER"
                     server_log.error(f"CONNECTION RESET BY PEER: {dict_of_clients[uid].ip_address}: CLOSING CONNECTION")
