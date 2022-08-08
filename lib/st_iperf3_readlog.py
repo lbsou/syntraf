29d96e808c0dc496eb3e8e041f73b659663bb25f
Louis-Berthier Soulliere <shadow131@hotmail.com>
2022-08-07 20:51:19 -0400

    fix outage mechanism

--- lib/st_iperf3_readlog.py
+++ lib/st_iperf3_readlog.py
@@ -27,11 +27,11 @@ def tail(file, interval, uid_client, uid_server, _config, listener_dict_key, dic
             # reading last line
             line = file.readline()
 
-            print(threads_n_processes)
+            log.debug(threads_n_processes)
             for obj_thread_n_process in threads_n_processes:
-                print(obj_thread_n_process.name + "   " + listener_dict_key)
+                log.debug(obj_thread_n_process.name + "   " + listener_dict_key)
                 if obj_thread_n_process.name == listener_dict_key:
-                    print("found!")
+                    log.debug("found!")
 
             '''
             Iperf3 stop generating events when the connection is lost for too long [how much exactly?], but we still want to report the losses
