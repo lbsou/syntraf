92d7e28c11c4b137de73aa19898d265cf06b3406
Louis-Berthier Soulliere <shadow131@hotmail.com>
2022-12-14 10:55:34 -0500

    OVERRIDE DEFAULT PORT RANGE PER CLIENT BIDIR CONNECTION

--- lib/st_iperf.py
+++ lib/st_iperf.py
@@ -37,8 +37,10 @@ def udp_hole_punch(dst_ip, dst_port, iperf3_pid, exit_boolean):
         time.sleep(1)
 
     print("SCAPY TIME")
-    scapy.send(scapy.IP(dst=dst_ip) / scapy.UDP(sport=max(lst_udp_port_iperf), dport=dst_port) / scapy.Raw(load="abc"),
-               loop=1, inter=0.1)
+    while not exit_boolean:
+        scapy.send(scapy.IP(dst=dst_ip) / scapy.UDP(sport=max(lst_udp_port_iperf), dport=dst_port) / scapy.Raw(load="KA"), loop=0, inter=0.1)
+        scapy.send(scapy.IP(dst=dst_ip) / scapy.UDP(sport=min(lst_udp_port_iperf), dport=dst_port) / scapy.Raw(load="KA"), count=1, loop=0, inter=0.1)
+        #scapy.send(scapy.IP(dst=dst_ip) / scapy.UDP(sport=max(lst_udp_port_iperf), dport=dst_port) / scapy.Raw(load="KA"), count=1, loop=0, inter=0.1)
 
 
 #################################################################################
