35e2765b1cb891f99da28ae8ab1af5bb19525a71
Louis-Berthier Soulliere <shadow131@hotmail.com>
2022-12-15 15:00:25 -0500

    OVERRIDE DEFAULT PORT RANGE PER CLIENT BIDIR CONNECTION

--- lib/st_iperf3_readlog.py
+++ lib/st_iperf3_readlog.py
@@ -52,7 +52,7 @@ def tail(file, interval, uid_client, uid_server, _config, edge_type, edge_dict_k
                                 cpt_port_bidir += 1
                             elif cpt_port_bidir == 1:
                                 iperf_read_log_thread.bidir_src_port = m.groups()[0]
-                                #log.error(f"GOT A PORT:{m.groups()[0]}")
+                                log.debug(f"GOT A PORT FOR UDP_HOLE_PUNCH:{m.groups()[0]}")
                                 cpt_port_bidir = 0
                         continue
                 else:
