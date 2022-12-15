321af86ee2c172c7b329ce0d1e20af4f2e15bcb4
Louis-Berthier Soulliere <shadow131@hotmail.com>
2022-12-15 14:41:55 -0500

    OVERRIDE DEFAULT PORT RANGE PER CLIENT BIDIR CONNECTION

--- lib/st_iperf.py
+++ lib/st_iperf.py
@@ -40,7 +40,8 @@ def udp_hole_punch(dst_ip, dst_port, iperf3_pid, exit_boolean, iperf_conn_thread
     while not exit_boolean[0]:
         # Send on all interface, dirty ack
         for if_name, addrs in interfaces.items():
-            iperf3_connectors_log.error(getattr(addrs,'address'))
+            iperf3_connectors_log.error(addrs)
+            #iperf3_connectors_log.error(getattr(addrs,'address'))
             for if_name2, stats2 in stats.items():
                 # Do not try to send on a down interface
                 if if_name2 == if_name and if_name2 != "lo":
