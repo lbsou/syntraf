802af9c2c8a445975b895798fafecde60b0bebd5
Louis-Berthier Soulliere <shadow131@hotmail.com>
2022-12-16 11:17:14 -0500

    OVERRIDE DEFAULT PORT RANGE PER CLIENT BIDIR CONNECTION

--- lib/st_process_and_thread.py
+++ lib/st_process_and_thread.py
@@ -223,9 +223,11 @@ def manage_mesh(config, threads_n_processes, mesh_type, obj_stats, config_file_p
 
 def thread_udp_hole(config, connector, connector_v, iperf3_pid, threads_n_processes, iperf_conn_thread):
     exit_boolean = [False]
-    log.error("===================================================================================")
-    log.error(f"{iperf_conn_thread}{connector_v}{connector}")
-    log.error("===================================================================================")
+
+    log.error("=================================")
+    log.error(f"{iperf_conn_thread.id}")
+    log.error("=================================")
+
     thread_run = threading.Thread(target=udp_hole_punch,
                                   args=(
                                       config['CONNECTORS'][connector]['DESTINATION_ADDRESS'],
