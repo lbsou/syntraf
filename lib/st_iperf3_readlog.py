368f902c5216520639526abc2dcea33e4c7188a6
Louis-Berthier Soulliere <shadow131@hotmail.com>
2022-12-16 20:50:43 -0500

    OVERRIDE DEFAULT PORT RANGE PER CLIENT BIDIR CONNECTION

--- lib/st_iperf3_readlog.py
+++ lib/st_iperf3_readlog.py
@@ -14,7 +14,7 @@ log = logging.getLogger("syntraf." + __name__)
 #################################################################################
 ### YIELD LINE FROM IPERF3 OUTPUT FILE
 #################################################################################
-def tail(file, interval, uid_client, uid_server, _config, edge_type, edge_dict_key, dict_data_to_send_to_server, threads_n_processes, iperf_read_log_thread=None):
+def tail(file, interval, uid_client, uid_server, _config, edge_type, edge_dict_key, dict_data_to_send_to_server, threads_n_processes, exit_boolean, iperf_read_log_thread=None):
     utime_last_event = 0
 
     try:
@@ -110,7 +110,6 @@ def tail(file, interval, uid_client, uid_server, _config, edge_type, edge_dict_k
     except Exception as exc:
         log.error(f"tail:{type(exc).__name__}:{exc}", exc_info=True)
 
-    log.error(f"UDP_HOLE_PUNCH FOR {edge_dict_key} TERMINATED")
 
 #################################################################################
 ###
@@ -190,9 +189,10 @@ def parse_line_to_array(line, _config, edge_dict_key, edge_type, dict_data_to_se
 
 
 #################################################################################
-### FUNCTION USE WITH THREAD TO READ LOGS
+### FUNCTION TO READ LISTENERS LOGS
 #################################################################################
-def read_log_listener(listener_dict_key, _config, stop_thread, dict_data_to_send_to_server, conn_db, threads_n_processes):
+def read_log_listener(listener_dict_key, _config, exit_boolean, dict_data_to_send_to_server, conn_db, threads_n_processes):
+    exit_message = "unknown"
     # Opening file and using generator
     pathlib.Path(os.path.join(_config['GLOBAL']['IPERF3_TEMP_DIRECTORY'], "syntraf_" + str(_config['LISTENERS'][listener_dict_key]['PORT']) + "_listener.log")).touch()
     file = open(
@@ -203,18 +203,25 @@ def read_log_listener(listener_dict_key, _config, stop_thread, dict_data_to_send
     try:
         for line in lines:
             #log.debug(f"TEMP DEBUG {line}")
-            if stop_thread[0] or not parse_line_to_array(line, _config, listener_dict_key, "LISTENERS", dict_data_to_send_to_server):
+            if exit_boolean[0] or not parse_line_to_array(line, _config, listener_dict_key, "LISTENERS", dict_data_to_send_to_server):
                 break
     except Exception as exc:
         log.error(f"read_log:{type(exc).__name__}:{exc}", exc_info=True)
     finally:
         file.close()
 
+    if exit_boolean:
+        exit_message = "Exit boolean became True"
+
+    log.error(f"READ_LOG FOR {listener_dict_key} TERMINATED. Exit message: {exit_message}")
+
 
 #################################################################################
-### FUNCTION USE WITH THREAD TO READ LOGS
+### FUNCTION TO READ CONNECTORS LOGS
 #################################################################################
-def read_log_connector(connector_dict_key, _config, stop_thread, dict_data_to_send_to_server, conn_db, threads_n_processes, iperf_read_log_thread):
+def read_log_connector(connector_dict_key, _config, exit_boolean, dict_data_to_send_to_server, conn_db, threads_n_processes, iperf_read_log_thread):
+    exit_message = "unknown"
+
     # Opening file and using generator
     pathlib.Path(os.path.join(_config['GLOBAL']['IPERF3_TEMP_DIRECTORY'], "syntraf_" + str(_config['CONNECTORS'][connector_dict_key]['PORT']) + "_connector.log")).touch()
     file = open(
@@ -225,11 +232,15 @@ def read_log_connector(connector_dict_key, _config, stop_thread, dict_data_to_se
     log.info(f"READING LOGS FOR CONNECTOR {connector_dict_key} FROM {file.name} ")
     try:
         for line in lines:
-            #log.error(line)
-            if stop_thread[0] or not parse_line_to_array(line, _config, connector_dict_key, "CONNECTORS", dict_data_to_send_to_server):
+            if exit_boolean[0] or not parse_line_to_array(line, _config, connector_dict_key, "CONNECTORS", dict_data_to_send_to_server):
                 break
 
     except Exception as exc:
         log.error(f"read_log:{type(exc).__name__}:{exc}", exc_info=True)
     finally:
         file.close()
+
+    if exit_boolean:
+        exit_message = "Exit boolean became True"
+
+    log.error(f"READ_LOG FOR {connector_dict_key} TERMINATED. Exit message: {exit_message}")
