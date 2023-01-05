b387b2fd0bceab0f34e8d5d27723351c8a99740c
Louis-Berthier Soulliere <shadow131@hotmail.com>
2023-01-04 21:26:43 -0500

    CHANGE HOW SYNTRAF ACQUIRE IPERF3 OUTPUT (--forceflush)

--- lib/st_iperf3_readlog.py
+++ lib/st_iperf3_readlog.py
@@ -26,93 +26,90 @@ def tail(interval, uid_client, uid_server, _config, edge_type, edge_dict_key, di
     try:
         cpt_port_bidir = 0
         for line in thr_iperf3.subproc.stdout:
-            lines = line.read().splitlines()
-
             if exit_boolean[0]:
                 yield "exit_boolean_true"
 
-            for line in lines:
-                values = line.split(" ")
+            values = line.split(" ")
 
-                if line:
-                    if (len(values) >= 20 and ("omitted" not in line) and ("terminated" not in line) and (
-                            "Interval" not in line) and ("receiver" not in line) and ("------------" not in line) and (
-                            "- - - - - - - - -" not in line)):
-                        utime_last_event = time.time()
-                        curr_thread.line_read += 1
-                        curr_thread.last_activity = datetime.now()
-                        yield line
+            if line:
+                if (len(values) >= 20 and ("omitted" not in line) and ("terminated" not in line) and (
+                        "Interval" not in line) and ("receiver" not in line) and ("------------" not in line) and (
+                        "- - - - - - - - -" not in line)):
+                    utime_last_event = time.time()
+                    curr_thread.line_read += 1
+                    curr_thread.last_activity = datetime.now()
+                    yield line
 
-                    else:
-                        log.debug(f"tail(): {edge_dict_key} - LINE DOES NOT CONTAIN METRICS:{line}")
-
-                        # When we have a bidir connection, iperf will open two port to destination. We want to grab the second source port, as it will allow us to keepalive the udp hole with scapy in another thread.
-                        #local 192.168.2.41 port 58743 connected to 192.168.6.100 port 15999
-                        #local 192.168.2.41 port 58744 connected to 192.168.6.100 port 15999
-
-                        m_lport = re.search(r"local (?:[0-9]{1,3}.){3}[0-9]{1,3} port (\d{1,10}) connected to (?:[0-9]{1,3}.){3}[0-9]{1,3} port \d{1,10}", line)
-                        m_laddr = re.search(r"local ((?:[0-9]{1,3}.){3}[0-9]{1,3}) port \d{1,10} connected to (?:[0-9]{1,3}.){3}[0-9]{1,3} port \d{1,10}", line)
-
-                        # Grab only the port from the second line, which is the RX
-                        if m_lport and cpt_port_bidir >= 0 and hasattr(iperf_read_log_thread, 'bidir_src_port'):
-                            if cpt_port_bidir == 0:
-                                cpt_port_bidir += 1
-                            elif cpt_port_bidir == 1:
-                                iperf_read_log_thread.bidir_src_port = int(m_lport.groups()[0])
-                                iperf_read_log_thread.bidir_local_addr = m_laddr.groups()[0]
-                                log.info(f"GOT A SRC_IP AND SRC_PORT FOR UDP_HOLE_PUNCH:{m_laddr.groups()[0]}/{m_lport.groups()[0]}")
-                                cpt_port_bidir = -1
-                        continue
                 else:
-                    #NO LINE
-                    utime_now = time.time()
-                    listener_just_started_or_absent = False
+                    log.debug(f"tail(): {edge_dict_key} - LINE DOES NOT CONTAIN METRICS:{line}")
 
-                    # Get the infos of the starttime of the current listener, if it has just started or does not exist, do no log an outage, it's just iperf that is not running.
-                    flag_no_thread_found = True
-                    for obj_thread_n_process in threads_n_processes:
-                        if obj_thread_n_process.name == edge_dict_key and (obj_thread_n_process.syntraf_instance_type == "LISTENER" or obj_thread_n_process.syntraf_instance_type == "CONNECTOR"):
-                            flag_no_thread_found = False
-                            dt_delta = datetime.now() - datetime.strptime(obj_thread_n_process.starttime,
-                                                                                            "%d/%m/%Y %H:%M:%S")
-                            if dt_delta.total_seconds() <= 60:
-                                listener_just_started_or_absent = True
+                    # When we have a bidir connection, iperf will open two port to destination. We want to grab the second source port, as it will allow us to keepalive the udp hole with scapy in another thread.
+                    #local 192.168.2.41 port 58743 connected to 192.168.6.100 port 15999
+                    #local 192.168.2.41 port 58744 connected to 192.168.6.100 port 15999
 
-                    if flag_no_thread_found: listener_just_started_or_absent = True
+                    m_lport = re.search(r"local (?:[0-9]{1,3}.){3}[0-9]{1,3} port (\d{1,10}) connected to (?:[0-9]{1,3}.){3}[0-9]{1,3} port \d{1,10}", line)
+                    m_laddr = re.search(r"local ((?:[0-9]{1,3}.){3}[0-9]{1,3}) port \d{1,10} connected to (?:[0-9]{1,3}.){3}[0-9]{1,3} port \d{1,10}", line)
 
-                    '''
-                    Iperf3 stop generating events when the connection is lost for too long [how much exactly?], but we still want to report the losses
-                    For that, we need to already have received a log in the past (utime_last_event != 0) and the current log file of iperf3 must not yield line (not line)
-                    '''
-                    #log.debug(f"OUTAGE_MECHANISM DEBUG utime_last_event:{utime_last_event}")
-                    #log.debug(f"{utime_last_event}{line}{listener_just_started_or_absent}")
-
-                    if utime_last_event != 0 and not line:
-                        log.debug(f"OUTAGE_MECHANISM DEBUG utime_now:{utime_now} utime_last_event:{utime_last_event} utime_now - utime_last_event: {(utime_now - utime_last_event)}")
-
-                        # If iperf3 did not write any events for the double of the interval he's supposed to
-                        if (utime_now - utime_last_event) >= (2 * interval):
-                            # Save new event to database with 100% loss for every time interval
-                            qty_of_event_to_report = (utime_now - utime_last_event) / interval
-                            log.warn(
-                                f"{edge_dict_key} - SYNTRAF HAS DETECTED AN OUTAGE, {qty_of_event_to_report} EVENTS WHERE LOST. GENERATING 100% LOSSES VALUES.")
-
-                            for utime_generated in range(int(utime_last_event) + interval, int(utime_now), interval):
-                                dt_generated = datetime.fromtimestamp(utime_generated)
-                                timezone = pytz.timezone(DefaultValues.TIMEZONE)
-                                dt_tz_generated = timezone.localize(dt_generated)
-                                timestamp_generated = dt_tz_generated.astimezone(pytz.timezone("UTC"))
-                                utime_generated_utc = dt_tz_generated.astimezone(pytz.timezone("UTC")).timestamp()
-
-                                # we could just yield a line, but that would required building a line with the same format as iperf3, it's a hack IMHO, prefer to save directly here.
-                                save_to_server([uid_client, uid_server, timestamp_generated, utime_generated_utc, "0", "0", "100"], _config,
-                                               edge_type, edge_dict_key, "0", "0", dict_data_to_send_to_server)
-
-                                log.debug(f"WRITING_TO_QUEUE ({len(dict_data_to_send_to_server)}) - {edge_dict_key}")
-                                log.debug(f"timestamp:{timestamp_generated}, bitrate: 0, jitter: 0, loss: 100, packet_loss: 0, packet_total: 0")
-
-                            utime_last_event = utime_now
+                    # Grab only the port from the second line, which is the RX
+                    if m_lport and cpt_port_bidir >= 0 and hasattr(iperf_read_log_thread, 'bidir_src_port'):
+                        if cpt_port_bidir == 0:
+                            cpt_port_bidir += 1
+                        elif cpt_port_bidir == 1:
+                            iperf_read_log_thread.bidir_src_port = int(m_lport.groups()[0])
+                            iperf_read_log_thread.bidir_local_addr = m_laddr.groups()[0]
+                            log.info(f"GOT A SRC_IP AND SRC_PORT FOR UDP_HOLE_PUNCH:{m_laddr.groups()[0]}/{m_lport.groups()[0]}")
+                            cpt_port_bidir = -1
                     continue
+            else:
+                #NO LINE
+                utime_now = time.time()
+                listener_just_started_or_absent = False
+
+                # Get the infos of the starttime of the current listener, if it has just started or does not exist, do no log an outage, it's just iperf that is not running.
+                flag_no_thread_found = True
+                for obj_thread_n_process in threads_n_processes:
+                    if obj_thread_n_process.name == edge_dict_key and (obj_thread_n_process.syntraf_instance_type == "LISTENER" or obj_thread_n_process.syntraf_instance_type == "CONNECTOR"):
+                        flag_no_thread_found = False
+                        dt_delta = datetime.now() - datetime.strptime(obj_thread_n_process.starttime,
+                                                                                        "%d/%m/%Y %H:%M:%S")
+                        if dt_delta.total_seconds() <= 60:
+                            listener_just_started_or_absent = True
+
+                if flag_no_thread_found: listener_just_started_or_absent = True
+
+                '''
+                Iperf3 stop generating events when the connection is lost for too long [how much exactly?], but we still want to report the losses
+                For that, we need to already have received a log in the past (utime_last_event != 0) and the current log file of iperf3 must not yield line (not line)
+                '''
+                #log.debug(f"OUTAGE_MECHANISM DEBUG utime_last_event:{utime_last_event}")
+                #log.debug(f"{utime_last_event}{line}{listener_just_started_or_absent}")
+
+                if utime_last_event != 0 and not line:
+                    log.debug(f"OUTAGE_MECHANISM DEBUG utime_now:{utime_now} utime_last_event:{utime_last_event} utime_now - utime_last_event: {(utime_now - utime_last_event)}")
+
+                    # If iperf3 did not write any events for the double of the interval he's supposed to
+                    if (utime_now - utime_last_event) >= (2 * interval):
+                        # Save new event to database with 100% loss for every time interval
+                        qty_of_event_to_report = (utime_now - utime_last_event) / interval
+                        log.warn(
+                            f"{edge_dict_key} - SYNTRAF HAS DETECTED AN OUTAGE, {qty_of_event_to_report} EVENTS WHERE LOST. GENERATING 100% LOSSES VALUES.")
+
+                        for utime_generated in range(int(utime_last_event) + interval, int(utime_now), interval):
+                            dt_generated = datetime.fromtimestamp(utime_generated)
+                            timezone = pytz.timezone(DefaultValues.TIMEZONE)
+                            dt_tz_generated = timezone.localize(dt_generated)
+                            timestamp_generated = dt_tz_generated.astimezone(pytz.timezone("UTC"))
+                            utime_generated_utc = dt_tz_generated.astimezone(pytz.timezone("UTC")).timestamp()
+
+                            # we could just yield a line, but that would required building a line with the same format as iperf3, it's a hack IMHO, prefer to save directly here.
+                            save_to_server([uid_client, uid_server, timestamp_generated, utime_generated_utc, "0", "0", "100"], _config,
+                                           edge_type, edge_dict_key, "0", "0", dict_data_to_send_to_server)
+
+                            log.debug(f"WRITING_TO_QUEUE ({len(dict_data_to_send_to_server)}) - {edge_dict_key}")
+                            log.debug(f"timestamp:{timestamp_generated}, bitrate: 0, jitter: 0, loss: 100, packet_loss: 0, packet_total: 0")
+
+                        utime_last_event = utime_now
+                continue
 
     except Exception as exc:
         log.error(f"tail:{type(exc).__name__}:{exc}", exc_info=True)
