38ab2a56096b70497941b1028afd8beb7cad7264
Louis-Berthier Soulliere <shadow131@hotmail.com>
2023-01-11 08:20:40 -0500

    removed iperf3 file output

--- lib/st_iperf3_readlog.py
+++ lib/st_iperf3_readlog.py
@@ -33,14 +33,13 @@ def tail(interval, uid_client, uid_server, _config, edge_type, edge_dict_key, di
     try:
         cpt_port_bidir = 0
 
-        #for line in thr_iperf3.subproc.stdout.read():
+        #for line in thr_iperf3.subproc.stdout:
         while True:
-            line, errs = thr_iperf3.subproc.communicate(timeout=1)
-
+            time.sleep(0.125)
+            line = thr_iperf3.subproc.stdout.read1(8888)
             line = line.decode('utf-8')
-            log.debug(f"LINE {edge_dict_key} {line}")
-
             if line:
+                log.debug(f"LINE {edge_dict_key} {line}")
                 values = line.split(" ")
                 if (len(values) >= 20 and ("omitted" not in line) and ("terminated" not in line) and (
                         "Interval" not in line) and ("receiver" not in line) and ("------------" not in line) and (
