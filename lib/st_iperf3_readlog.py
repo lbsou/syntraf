64b96ac78a0fceebda9130a68fae56f1f6893576
Louis-Berthier Soulliere <shadow131@hotmail.com>
2023-01-10 21:06:20 -0500

    removed iperf3 file output

--- lib/st_iperf3_readlog.py
+++ lib/st_iperf3_readlog.py
@@ -50,7 +50,6 @@ def tail(interval, uid_client, uid_server, _config, edge_type, edge_dict_key, di
                 elif "TX-C" in line and "TX-S" in line:
                     continue
                 elif "connected to" in line and "local" in line:
-                    log.debug(f"TRYING TO GRAB SOURCE PORT")
                     # When we have a bidir connection, iperf will open two port to destination. We want to grab the second source port, as it will allow us to keepalive the udp hole with scapy in another thread.
                     #local 192.168.2.41 port 58743 connected to 192.168.6.100 port 15999
                     #local 192.168.2.41 port 58744 connected to 192.168.6.100 port 15999
