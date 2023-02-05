e5cb0d3b14fbd12248fec891a6e425dfe8a5d87c
Louis-Berthier Soulliere <shadow131@hotmail.com>
2023-02-04 20:31:19 -0500

    removed iperf3 file output

--- syntraf.py
+++ syntraf.py
@@ -208,7 +208,7 @@ def run():
         f.close()
 
         for thread in threading.enumerate():
-            log.error(f"{thread.name}---{thread.is_alive()})
+            log.error(f"{thread.name}---{thread.is_alive()}")
 
         # # Validate if reload flag has been set by user with another instance of the script (-r)
         # try:
