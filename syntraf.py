a640d3cad5435162ff89242485859d5544388c1b
Louis-Berthier Soulliere <shadow131@hotmail.com>
2023-02-22 14:15:37 -0500

    working on outage

--- syntraf.py
+++ syntraf.py
@@ -237,8 +237,8 @@ def run():
 
         f = open(os.path.join(DefaultValues.SYNTRAF_ROOT_DIR, "real_thread_status.txt"), "w")
         for thread in threading.enumerate():
-            f.write(f"{thread.name}---{thread.is_alive()}")
-        f.write("\n")
+            f.write(f"{thread.name}")
+            f.write("\n")
         f.close()
 
         # # Validate if reload flag has been set by user with another instance of the script (-r)
