d2103aa25199c657e3e3b9523b1142df9d0098d9
Louis-Berthier Soulliere <shadow131@hotmail.com>
2023-02-21 19:51:29 -0500

    working on outage

--- lib/st_iperf3_readlog.py
+++ lib/st_iperf3_readlog.py
@@ -17,11 +17,9 @@ def read_log(edge_key, edge_type, config, dict_data_to_send_to_server, threads_n
         lines = tail(edge_type, edge_key, iperf3_thread, exit_boolean)
         log.info(f"READING LOGS FOR {edge_type} {edge_key}")
         while True:
-            log.debug("PARSING LOG")
             if exit_boolean[0]:
                 return
             line = next(lines, None)
-            log.debug(f"line{edge_type}")
             if line:
                 if not parse_line(line, config, edge_key, edge_type, threads_n_processes, dict_data_to_send_to_server):
                     break
@@ -64,7 +62,7 @@ def tail(edge_type, edge_key, thr_iperf3, exit_boolean):
                     if "TX-C" in line or "TX-S" in line:
                         continue
                     else:
-                        log.debug(f"LINE FROM {edge_type} : {edge_key} - {line}")
+                        log.debug(f"LINE FROM {edge_type} : {edge_key} - {line} - {datetime.now()}")
                         yield line
 
     except Exception as exc:
