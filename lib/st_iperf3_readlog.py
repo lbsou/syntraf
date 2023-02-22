8d089936a3e07b58e551d4c8822da9d09c33d7c1
Louis-Berthier Soulliere <shadow131@hotmail.com>
2023-02-22 13:49:22 -0500

    working on outage

--- lib/st_iperf3_readlog.py
+++ lib/st_iperf3_readlog.py
@@ -42,7 +42,7 @@ def tail(config, edge_type, edge_key, exit_boolean, threads_n_processes):
         while iperf3_obj_process_n_thread.subproc is None:
             if exit_boolean[0]:
                 return
-            time.sleep(config['LISTENERS'][edge_key]['INTERVAL'])
+            time.sleep(int(config[f"{edge_type}s"][edge_key]['INTERVAL']) / 2)
 
         log.debug(f"READLOG THREAD ACQUIRED IPERF3 STDOUT FOR {edge_type} - {iperf3_obj_process_n_thread.name} -  {edge_key}")
 
